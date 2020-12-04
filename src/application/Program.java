4d05d3120a76548139d247b922a81f581acfb3bf
KellyCarvalho <18kellycarvalho@gmail.com>
2020-12-04 19:07:48 -0300

    Solução Final

--- src/application/Program.java
+++ src/application/Program.java
@@ -31,8 +31,8 @@ public class Program {
 			
 		}
 		
-		Comparator<Produto>comp = (p1,p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-		list.sort(comp);
+		
+		list.sort((p1,p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
 		for(Produto p:list) {
 			System.out.println(p);
 		}
