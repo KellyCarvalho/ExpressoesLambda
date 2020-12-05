6a3d3bd102385def43288b222ac44b30a16185d5
KellyCarvalho <18kellycarvalho@gmail.com>
2020-12-04 22:04:51 -0300

    pipeline

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,48 +12,27 @@ import java.util.stream.Stream;
 
 
 
-import entities.Produto;
-import model.services.ServiceProduto;
-import util.UpperCaseNome;
+
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Informe quantos produtos deseja inserir");
-		int n =sc.nextInt();
 		
-		List<Produto> list = new ArrayList<>();
+		List<Integer> l = Arrays.asList(1,2,3,4,5,6,7,8,9);
 		
-		int count=1;
-		for(int i=0;i<n;i++) {
+		List<Integer> newList = l.stream()
+				.filter(a ->a %2==0)
+				.map(a -> a * 10)
+				.collect(Collectors.toList());
 			
-			System.out.println("Informe o nome do "+count+"º produto");
-			String name =sc.next();
-			System.out.println("Informe o preco do "+count+"º produto");
-			double price =sc.nextDouble();
-			count++;
-		 
-			list.add(new Produto(name,price));
-			
-		}
-	
-		Stream<Double> p = list.stream().map(x->x.getPrice()*15);//gerou um stream com os resultados
-		System.out.println(Arrays.toString(p.toArray()));
-		
-		Stream<String> produtos = Stream.of("Notebook","Televisão","Geladeira");
-		System.out.println(Arrays.toString(produtos.toArray()));
-		
-		Stream<Integer> bonus = Stream.iterate(0, x->x+1);
-		System.out.println(Arrays.toString(bonus.limit(10).toArray()));
+				
 		
 		
 		
-		sc.close();
+		
 
 	}
 
